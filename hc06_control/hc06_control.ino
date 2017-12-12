62ead21f8f57760dae974560fafac17d57728f22
José Almeida <jose.afga@gmail.com>
2017-12-12 00:10:19 -0200

    add beep

--- hc06_control/hc06_control.ino
+++ hc06_control/hc06_control.ino
@@ -24,6 +24,7 @@ int enpin[2] = {0, 1}; // EN: Status of switches output (Analog pin)
 // pins
 const byte ledPin = 13;
 const byte buzzerPin = 12;
+const byte powerPin = 11;
 const byte btTx = 2;
 const byte btRx = 3;
 
@@ -36,6 +37,7 @@ bool buzzerStatus = false; // buzzer
 // timer
 unsigned long timer;
 const long timeout = 3000;
+bool stopped = true;
 // variables to receive parsed commands
 int motor0 = 0;
 int motor1 = 0;
@@ -56,10 +58,17 @@ void setup() {
 
   pinMode(ledPin, OUTPUT); // LED
   pinMode(buzzerPin, OUTPUT); // Buzzer
+  pinMode(powerPin, OUTPUT); // To keep arduino on
+  // default state
+  digitalWrite(ledPin, LOW);
+  digitalWrite(buzzerPin, LOW);
+  digitalWrite(powerPin, HIGH);
 
   timer = millis(); // set initial timer
 
+  
   Serial.println("<Arduino is ready>");
+  beep(1);
 }
 
 void loop() {
@@ -74,20 +83,6 @@ void loop() {
   // getDataFromPC();
   // updateMotors();
   updateTimer();
-}
-
-
-// check timer status
-void updateTimer() {
-  if ((millis() - timer) > timeout) {
-    if (debug) {
-      Serial.println(String("Timeout stopping"));
-    }
-
-    motorOff(0);
-    motorOff(1);
-    delay(200);
-  }
 }
 
 
@@ -120,6 +115,10 @@ void getDataFromPC(char x) {
 void parseData() {
   char * strtokIndx; // this is used by strtok() as an index
   char cmd[2];
+
+  if (debug) {
+    Serial.println(inputBuffer);
+  }
   
   strtokIndx = strtok(inputBuffer,",");      // get the first part - the string
   strcpy(cmd, strtokIndx); // copy it to messageFromPC
@@ -146,6 +145,37 @@ void parseData() {
     default:
       // optional
       break;
+  }
+}
+
+
+// check timer status
+void updateTimer() {
+  if ((millis() - timer) > timeout) {
+    if(!stopped) {
+      if (debug) {
+        Serial.println(String("Timeout stopping"));
+      }
+      
+      stopped = true;
+  
+      motorOff(0);
+      motorOff(1);
+      beep(3);
+    }
+  } else {
+    stopped = false;
+  }
+}
+
+
+// execute buzzer beeps x times
+void beep(int times) {
+  for(int i = 0; i < times; ++i) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(50);
+    digitalWrite(buzzerPin, LOW);
+    delay(100);
   }
 }
 
@@ -178,10 +208,6 @@ void updateMotors() {
     motorGo(1, CCW, abs(motor1));
   } else {
     motorOff(1);
-  }
-
-  if (debug) {
-    Serial.println(String("M: ") + motor0 + String(", ") + motor1);
   }
 
   if ((analogRead(cspin[0]) > CS_THRESHOLD) && (analogRead(cspin[1]) > CS_THRESHOLD)) {
